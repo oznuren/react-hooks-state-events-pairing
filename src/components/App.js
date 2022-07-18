65c6346871db483dddede21ee0e5d1f55f50926d
oznuren <95101906+oznuren@users.noreply.github.com>
2022-07-18 21:48:16 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,21 @@
 import video from "../data/video.js";
+import React, { useState } from "react";
 
 function App() {
   console.log("Here's your data:", video);
+  const [upvote, setUpvote] = useState(video.upvotes);
+  const [downvote, setDownvote] = useState(video.downvotes);
+  const [comments, setComments] = useState(false);
+
+  const handleUpvote = () => {
+    setUpvote(upvote + 1);
+  };
+  const handleDownvote = () => {
+    setDownvote(downvote - 1);
+  };
+  const handleComments = () => {
+    setComments(!comments);
+  };
 
   return (
     <div className="App">
@@ -13,6 +27,49 @@ function App() {
         allowFullScreen
         title="Thinking in React"
       />
+      <div key={video.id}>
+        <h1>{video.title}</h1>
+        <p>
+          {video.views} Views | Uploaded{video.createdAt}
+        </p>
+
+        <button
+          style={{ margin: "3px", padding: "5px" }}
+          onClick={handleUpvote}
+        >
+          {upvote} 👍
+        </button>
+        <button
+          style={{ margin: "3px", padding: "5px" }}
+          onClick={handleDownvote}
+        >
+          {downvote} 👎
+        </button>
+        <br />
+        <button
+          style={{ margin: "3px", padding: "5px" }}
+          onClick={handleComments}
+        >
+          {comments ? "Hide Comments" : "Show Comments"}
+        </button>
+        <hr></hr>
+
+        {comments && (
+          <div>
+            <p>{video.comments.length} Comments</p>
+            <p>
+              {video.comments.map((comment) => (
+                <div key={comment.id}>
+                  <p>
+                    <strong>{comment.user}</strong>
+                    <br /> {comment.comment}
+                  </p>
+                </div>
+              ))}
+            </p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
